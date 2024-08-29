109967d67c266735e3f5e7fe415936f1fca809c2
unknown <bhim magar>
2024-08-29 17:04:54 +0545

    third commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// console.log("welcome to java script");
+
+// lets create some functions
+function mouse () {
+    console.log("my mouse is perfect");
+} 
+function keyboard () {
+    console.log("my keyboard button stuck");
+}
+function main () {
+    mouse();
+    keyboard();
+}
+main();
